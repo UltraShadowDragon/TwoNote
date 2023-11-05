4d5e922071f6ba09fba081d922361f494ab3d587
Abdullah <usmaniabdullah91@gmail.com>
2023-11-05 16:50:12 +0530

    Serving from dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   devtool: 'eval-source-map',
-  entry: './src/index.js',
+  entry: './src/js/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    filename: 'js/index.js'
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'css/style.css',
+      template: './src/css/style.css',
+    })
+  ]
 };
